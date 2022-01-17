43bc3dd991950c9e0ee34825ddcba3ded29f0983
enjuro <enjuro0926@gmail.com>
2022-01-17 05:04:34 +0000

    layout adjustment and tweet model modification

--- db/migrate/20220113022052_create_favorites.rb
+++ db/migrate/20220113022052_create_favorites.rb
@@ -1,10 +1,11 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.integer :user_id
-      t.integer :tweet_id
+      t.integer :user_id, foreign_key: true
+      t.integer :tweet_id, foreign_key: true
 
       t.timestamps
+      t.index [:user_id, :tweet_id], unique: true
     end
   end
 end
